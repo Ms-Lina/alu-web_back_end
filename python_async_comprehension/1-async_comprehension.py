b31d8b74a1aa6f9cae6fb8cd239ce8ad8f3c106e
Ms-Lina <139906763+Ms-Lina@users.noreply.github.com>
2025-05-20 19:54:00 +0200

    Updated

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,3 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Async comprehension"""
     return [i async for i in async_generator()]
+    
